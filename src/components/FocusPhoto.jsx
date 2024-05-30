35c537dba3d237fdf24ef2f9fccf6c25ca3a97c4
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-30 12:28:47 +0800

    Fix functionality of FocusPhoto
    Bug: multiple event listeners were being added every time the image was clicked, this was removed

--- src/components/FocusPhoto.jsx
+++ src/components/FocusPhoto.jsx
@@ -1,19 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Image1 from "../assets/images/Rectangle 72.png";
 import Image2 from "../assets/images/Rectangle 7.png";
 import "../styles/FocusPhoto.css";
 
 const FocusPhoto = () => {
-  const translateImage = () => {
-    const image = document.querySelector(".photo-container");
-
-    const handleClick = (event) => {
-      if (event.target.classList.contains("focus-photo")) {
-        console.log("Clicked element:", event.target);
-      }
-    };
-
-    image.addEventListener("click", handleClick);
+  const translateImage = (event) => {
+    if (event.target.classList.contains("focus-photo")) {
+      event.target.classList.toggle("centered");
+      console.log("Clicked element:", event.target);
+    }
   };
 
   return (
