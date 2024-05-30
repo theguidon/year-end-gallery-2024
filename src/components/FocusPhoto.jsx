7f2fbd8ca3647d4c3387e8cdc161c89a5fe8dd60
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-30 21:25:34 +0800

    Add FocusPhoto translate to center functionality

--- src/components/FocusPhoto.jsx
+++ src/components/FocusPhoto.jsx
@@ -6,7 +6,20 @@ import "../styles/FocusPhoto.css";
 const FocusPhoto = () => {
   const translateImage = (event) => {
     if (event.target.classList.contains("focus-photo")) {
-      event.target.classList.toggle("centered");
+      // event.target.classList.toggle("centered");
+      const rect = event.target.getBoundingClientRect();
+      const newImg = document.createElement("img");
+      newImg.setAttribute("src", event.target.src);
+      newImg.setAttribute("alt", "Year End Gallery 2023-2024 Photo");
+      newImg.setAttribute("id", "new-img");
+      newImg.setAttribute("class", "centered");
+      newImg.style = `visibility: hidden;`;
+      document.querySelector("body").appendChild(newImg);
+      const rect2 = newImg.getBoundingClientRect();
+      event.target.style = `transform: translate(${rect2.x - rect.x}px, ${
+        rect2.y - rect.y
+      }px);`;
+      document.getElementById("new-img").remove();
       console.log("Clicked element:", event.target);
     }
   };
