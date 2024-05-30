1d04c6e25b867be66e96a135c3be7f00295cdd5d
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-31 00:21:56 +0800

    Add FocusPhotoText component on image click

--- src/components/FocusPhoto.jsx
+++ src/components/FocusPhoto.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import FocusPhotoText from "./FocusPhotoText";
-import Image1 from "../assets/images/Rectangle 72.png";
 import Image2 from "../assets/images/Rectangle 7.png";
 import "../styles/FocusPhoto.css";
 
 const FocusPhoto = () => {
+  const [focus, setFocus] = useState(true);
+
+  const focusPhoto = (event) => {
+    setFocus(!focus);
+    if (event.target.classList.contains("photo")) {
+      if (focus) {
+        document.querySelector(".photo-text").style = `display: flex;`;
+      } else {
+        document.querySelector(".photo-text").style = `display: none;`;
+      }
+      console.log("Clicked element:", event.target);
+    }
+  };
+
   return (
     <div className="photo-container">
-      <img
-        className="photo"
-        src={Image2}
-        alt="Year End Gallery 2023-2024 Photo"
-      />
-      <FocusPhotoText />
+      <div className="photos">
+        <img
+          className="photo"
+          src={Image2}
+          alt="Year End Gallery 2023-2024 Photo"
+          onClick={focusPhoto}
+        />
+        <FocusPhotoText />
+      </div>
     </div>
   );
 };
