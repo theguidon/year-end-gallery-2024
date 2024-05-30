2375ac2d8319a7cdd96d32535b274bf3b99edc79
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-30 23:25:08 +0800

    Add boilerplate for FocusPhotoText component
    Also removed animations for now

--- src/components/FocusPhoto.jsx
+++ src/components/FocusPhoto.jsx
@@ -1,43 +1,18 @@
 import React, { useState } from "react";
+import FocusPhotoText from "./FocusPhotoText";
 import Image1 from "../assets/images/Rectangle 72.png";
 import Image2 from "../assets/images/Rectangle 7.png";
 import "../styles/FocusPhoto.css";
 
 const FocusPhoto = () => {
-  const translateImage = (event) => {
-    if (event.target.classList.contains("focus-photo")) {
-      // event.target.classList.toggle("centered");
-      const rect = event.target.getBoundingClientRect();
-      const newImg = document.createElement("img");
-      newImg.setAttribute("src", event.target.src);
-      newImg.setAttribute("alt", "Year End Gallery 2023-2024 Photo");
-      newImg.setAttribute("id", "new-img");
-      newImg.setAttribute("class", "centered");
-      newImg.style = `visibility: hidden;`;
-      document.querySelector("body").appendChild(newImg);
-      const rect2 = newImg.getBoundingClientRect();
-      event.target.style = `transform: translate(${rect2.x - rect.x}px, ${
-        rect2.y - rect.y
-      }px);`;
-      document.getElementById("new-img").remove();
-      console.log("Clicked element:", event.target);
-    }
-  };
-
   return (
     <div className="photo-container">
       <img
-        className="focus-photo"
-        src={Image1}
-        alt="Year End Gallery 2023-2024 Photo"
-        onClick={translateImage}
-      />
-      <img
-        className="focus-photo"
+        className="photo"
         src={Image2}
         alt="Year End Gallery 2023-2024 Photo"
-        onClick={translateImage}
       />
+      <FocusPhotoText />
     </div>
   );
 };
---
+++ src/components/FocusPhotoText.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/FocusPhotoText.css";
+
+const FocusPhotoText = () => {
+  return <div>FocusPhotoText</div>;
+};
+
+export default FocusPhotoText;
