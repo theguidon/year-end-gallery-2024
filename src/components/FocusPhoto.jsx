95cae21c62d5cc8e30783e59d4932cd9cb3e55cd
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-30 10:54:52 +0800

    Add functionality to target correct photos

--- src/components/FocusPhoto.jsx
+++ src/components/FocusPhoto.jsx
@@ -1,8 +1,37 @@
 import React from "react";
-import styles from "../styles/FocusPhoto.module.css";
+import Image1 from "../assets/images/Rectangle 72.png";
+import Image2 from "../assets/images/Rectangle 7.png";
+import "../styles/FocusPhoto.css";
 
 const FocusPhoto = () => {
-  return <div>FocusPhoto</div>;
+  const translateImage = () => {
+    const image = document.querySelector(".photo-container");
+
+    const handleClick = (event) => {
+      if (event.target.classList.contains("focus-photo")) {
+        console.log("Clicked element:", event.target);
+      }
+    };
+
+    image.addEventListener("click", handleClick);
+  };
+
+  return (
+    <div className="photo-container">
+      <img
+        className="focus-photo"
+        src={Image1}
+        alt="Year End Gallery 2023-2024 Photo"
+        onClick={translateImage}
+      />
+      <img
+        className="focus-photo"
+        src={Image2}
+        alt="Year End Gallery 2023-2024 Photo"
+        onClick={translateImage}
+      />
+    </div>
+  );
 };
 
 export default FocusPhoto;
