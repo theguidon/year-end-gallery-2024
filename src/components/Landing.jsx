73d9efbe09129e3a63d7e6331ba3f667620c0534
Arthur Lluisma <ajclluisma@gmail.com>
2024-06-02 20:31:38 +0800

    Add Landing content

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,12 +4,14 @@ import LandingMenu from "../assets/images/LandingMenu.svg"
 import LandingBatteryIcon from "../assets/images/LandingBatteryIcon.svg"
 import LandingBottomLeftText1 from "../assets/images/F 3.5.svg"
 import LandingBottomLeftText2 from "../assets/images/ISO 100.svg"
+import Logo from "../assets/images/Logo.png"
+import StartButton from "../assets/images/StartButton.svg"
 import "../styles/Landing.css"
 
 const Landing = () => {
   return (
     <div className="landing">
-      <div className="landing-bg-container">
+      <div className="landing-container">
         <div className="landing-bg">
           <img className="landing-img" src={LandingImage} alt="Landing Photo" />
           <div className="landing-frame"></div>
@@ -21,8 +23,13 @@ const Landing = () => {
           </div>
           <div className="landing-inner-frame"></div>
         </div>
+        <div className="landing-hero">
+          <img src={Logo} alt="The GUIDON" />
+          <p>Year End Gallery</p>
+          <p>2023-2024</p>
+          <img src={StartButton} alt="Start" />
+        </div>
       </div>
-      <div className=""></div>
     </div>
   )
 }
